f3b564429798114ffd75b2306a3d04300ab45147
mlanciau <mlanciau@gmail.com>
2020-03-24 17:51:45 +0100

    Adding dataflow and composer

---
+++ composer/twitter_google_cloud.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import airflow
+from airflow import DAG
+#from airflow.operators import python_operator
+#from airflow.contrib.operators import gcs_to_gcs
+#from airflow.contrib.operators import ssh_operator #sshtunnel, paramiko
+from airflow.contrib.operators import bigquery_operator
+from airflow.contrib.operators import dataflow_operator
+from airflow.operators import bash_operator
+from datetime import datetime, timedelta
+from airflow.models import Variable
+#import twitter # library installed directly into the environment via the pypi tab
+#from google.cloud import storage
+import json
+from time import time
+
+default_args = {
+    'start_date': datetime(2020, 3, 15),
+    'schedule_interval': '@daily',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+    'depends_on_past': False,
+    'email': ['mlanciau+airflow@google.com'],
+    'email_on_failure': True,
+    'email_on_retry': False
+}
+
+dag = DAG(
+    'twitter_search',
+    default_args=default_args,
+    description='Load data from GCS to BQ Serving Layer',
+    dagrun_timeout=timedelta(minutes=60)
+)
+
+load_raw_data = dataflow_operator.DataFlowPythonOperator(
+    task_id='load_raw_data',
+    dag=dag,
+    py_file='/home/airflow/gcs/dags/dataflow/twitter-google-dataflow.py',
+    #py_file='dataflow/twitter-google-dataflow.py',
+    job_name='twitter-google-dataflow-{{ ds }}',
+    dataflow_default_options={'project':'${GCP_PROJECT}', 'region': 'europe-west1','zone':'europe-west6-a','runner':'DataflowRunner'},
+    options={'job_date':'{{ ds }}', 'twitter_bucket':'${twitter_bucket}', 'dataflow_bucket':'${dataflow_bucket}'}
+)
+
+#delete_sl_partition = bigquery_operator.BigQueryOperator( # TODO change to bq command line
+#    task_id='delete_sl_partition',
+#    dag=dag,
+#    sql='''DELETE FROM dataops_demo_sl_dev.t_twitter_google WHERE c_created = '{{ ds }}' ''',
+#    use_legacy_sql=False
+#)
+
+delete_sl_partition = bash_operator.BashOperator(
+    task_id='delete_sl_partition',
+    dag=dag,
+    bash_command='''bq rm -f -t 'dataops_demo_sl_dev.t_twitter_google${{ macros.ds_format(ds, "%Y-%m-%d", "%Y%m%d") }}' ''',
+)
+
+from_raw_to_sl = bigquery_operator.BigQueryOperator(
+    task_id='from_raw_to_sl',
+    dag=dag,
+    sql='''SELECT PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS c_timestamp, CAST(PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS date) as c_created, id, user_id, user_name, lang, user_screen_name, text
+        FROM dataops_demo_raw_dev.t_twitter_google_{{ macros.ds_format(ds, "%Y-%m-%d", "%Y_%m_%d") }}''',
+    destination_dataset_table='dataops_demo_sl_dev.t_twitter_google',
+    write_disposition='WRITE_APPEND',
+    create_disposition='CREATE_IF_NEEDED',
+    time_partitioning={'type':'DAY', 'field':'c_created'},
+    allow_large_results=True,
+    use_legacy_sql=False
+)
+
+load_raw_data >> delete_sl_partition >> from_raw_to_sl
---
+++ dataflow/twitter-google-dataflow.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import apache_beam as beam
+import argparse
+from math import trunc
+import time
+import json
+
+def ExtractFields(line):
+   record = json.loads(line)
+   new_record = {}
+   new_record['created_at'] = record['created_at']
+   new_record['lang'] = record['lang']
+   new_record['id'] = record['id']
+   new_record['user_id'] = record['user']['id']
+   new_record['user_name'] = record['user']['name']
+   new_record['user_screen_name'] = record['user']['screen_name']
+   new_record['text'] = record['text']
+   new_record['json'] = json.dumps(record)
+   return new_record
+
+known_args = None
+pipeline_args = None
+
+def run():
+   #job_name = 'twitter-google-dataflow-' + str(trunc(time.time()))
+
+   argv = [
+      #'--job_name=' + job_name,
+      '--save_main_session',
+      '--staging_location=gs://' + str(known_args.dataflow_bucket) + '/staging/',
+      '--temp_location=gs://' + str(known_args.dataflow_bucket) + '/temp/',
+      #'--runner=DataflowRunner',
+	  #'--region=europe-west1',
+	  #'--zone=europe-west6-a',
+	  '--max_num_workers=4'#,
+      #'--network=dev-vpc'
+   ]
+
+   p = beam.Pipeline(argv=argv + pipeline_args)
+   #print('Pipeline is being created')
+   input = 'gs://' + str(known_args.twitter_bucket) + '/twitter/google/' + str(known_args.job_date) + '/'
+
+   (p
+      | 'ReadMyFile' >> beam.io.ReadFromText(input)
+      | 'ExtractFields' >> beam.Map(ExtractFields)
+      | 'write' >> beam.io.WriteToBigQuery(
+        table='dataops_demo_raw_dev.t_twitter_google_' + known_args.job_date.replace('-','_'),
+        schema='created_at:STRING, lang:STRING, id:NUMERIC, user_id:NUMERIC, user_name:STRING, user_screen_name:STRING, text:STRING, json:STRING',
+        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
+      )
+   )
+
+   p.run().wait_until_finish()
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--job_date')
+   parser.add_argument('--twitter_bucket')
+   parser.add_argument('--dataflow_bucket')
+   known_args, pipeline_args = parser.parse_known_args()
+   #print(known_args)
+   #print(pipeline_args)
+   run()
