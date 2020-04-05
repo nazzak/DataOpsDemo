083f5b14470856be79341a8835ebce52839ca133
mlanciau <mlanciau@gmail.com>
2020-04-05 17:16:44 +0200

    Update twitter_spark_etl.py

--- composer/twitter_spark_etl.py
+++ composer/twitter_spark_etl.py
@@ -52,7 +52,7 @@ create_dataproc_cluster = dataproc_operator.DataprocClusterCreateOperator(
     worker_machine_type='n1-standard-1',
     graceful_decommission_timeout='1h',
     image_version='1.4',
-    storage_bucket='gs://{{ var.value.v_twitter_temp_bucket }}',
+#    storage_bucket='gs://{{ var.value.v_twitter_temp_bucket }}',
     subnetwork_uri='10.172.0.0/20',
     internal_ip_only=True
 )
