9adf79412ba225c6cac419c055c0c85655d759b3
Maxime Lanciaux <mlanciau@gmail.com>
2020-04-22 18:54:24 +0200

    Adding bigquery update schema

--- composer/migration_twitter_google_cloud.py
+++ composer/migration_twitter_google_cloud.py
@@ -16,20 +16,21 @@
 
 import airflow
 from airflow import DAG
-#from airflow.operators import python_operator
-#from airflow.contrib.operators import gcs_to_gcs
-#from airflow.contrib.operators import ssh_operator #sshtunnel, paramiko
+from airflow.operators import python_operator
+# from airflow.contrib.operators import gcs_to_gcs
+# from airflow.contrib.operators import ssh_operator #sshtunnel, paramiko
 from airflow.contrib.operators import bigquery_operator
-from airflow.contrib.operators import dataflow_operator
+# from airflow.contrib.operators import dataflow_operator
 from airflow.operators import bash_operator
 from datetime import datetime, timedelta
-from airflow.models import Variable
-from airflow.operators import dagrun_operator
-#import twitter # library installed directly into the environment via the pypi tab
-#from google.cloud import storage
-import json
-from time import time
-import os
+# from airflow.models import Variable
+# from airflow.operators import dagrun_operator
+# import twitter # library installed directly into the environment via the pypi tab
+# from google.cloud import storage
+from google.cloud import bigquery
+# import json
+# from time import time
+# import os
 
 default_args = {
     'start_date': datetime(2020, 3, 15),
@@ -42,31 +43,34 @@ default_args = {
     'email_on_retry': False
 }
 
+def bigquery_schema_update(**kwargs):
+    client = bigquery.Client()
+    table = client.get_table('dataops_demo_sl_dev.t_twitter_google')
+    new_column_name = "source"
+    original_schema = table.schema
+    new_schema = original_schema[:]
+    if new_column_name not in new_schema:
+        new_schema.append(bigquery.SchemaField(new_column_name, "STRING"))
+        table.schema = new_schema
+        table = client.update_table(table, ["schema"])
+    return(new_column_name) # hint push the return value to the XCOM
+
 dag = DAG(
     'migration_twitter_search_01',
     default_args=default_args,
     description='Migration with data replay with BigQuery Schema change',
     schedule_interval='@daily',
+    catchup=True,
     dagrun_timeout=timedelta(minutes=30),
     max_active_runs=1
 )
 
-#load_raw_data = dataflow_operator.DataFlowPythonOperator(
-#    task_id='load_raw_data',
-#    dag=dag,
-#    py_file='/home/airflow/gcs/dags/dataflow/twitter-google-dataflow.py',
-    #py_file='dataflow/twitter-google-dataflow.py',
-#    job_name='twitter-google-dataflow-{{ ds }}',
-#    dataflow_default_options={'project':os.environ.get('GCP_PROJECT'), 'region': 'europe-west1','zone':'europe-west6-a','runner':'DataflowRunner'},
-#    options={'job_date':'{{ ds }}', 'twitter_bucket':os.environ.get('TWITTER_BUCKET'), 'dataflow_bucket':os.environ.get('DATAFLOW_BUCKET')}
-#)
-
-#delete_sl_partition = bigquery_operator.BigQueryOperator( # TODO change to bq command line
-#    task_id='delete_sl_partition',
-#    dag=dag,
-#    sql='''DELETE FROM dataops_demo_sl_dev.t_twitter_google WHERE c_created = '{{ ds }}' ''',
-#    use_legacy_sql=False
-#)
+bigquery_schema_update = python_operator.PythonOperator(
+    task_id='bigquery_schema_update',
+    dag=dag,
+    python_callable=bigquery_schema_update,
+    provide_context=True
+)
 
 delete_sl_partition = bash_operator.BashOperator(
     task_id='delete_sl_partition',
@@ -78,22 +82,24 @@ delete_sl_partition = bash_operator.BashOperator(
 from_raw_to_sl = bigquery_operator.BigQueryOperator(
     task_id='from_raw_to_sl',
     dag=dag,
-    sql='''SELECT PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS c_timestamp, CAST(PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS date) as c_created, id, user_id, user_name, lang, user_screen_name, text, JSON_EXTRACT(json, '$.source') AS source
+    sql='''SELECT PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS c_timestamp,
+        CAST(PARSE_TIMESTAMP('%a %b %d %H:%M:%S +0000 %E4Y', created_at) AS date) as c_created, id, user_id, user_name,
+        lang, user_screen_name, text, JSON_EXTRACT(json, '$.source') AS source
         FROM dataops_demo_raw_dev.t_twitter_google_{{ macros.ds_format(ds, "%Y-%m-%d", "%Y_%m_%d") }}''',
     destination_dataset_table='dataops_demo_sl_dev.t_twitter_google',
     write_disposition='WRITE_APPEND',
     create_disposition='CREATE_IF_NEEDED',
-    time_partitioning={'type':'DAY', 'field':'c_created'},
+    time_partitioning={'type': 'DAY', 'field': 'c_created'},
     allow_large_results=True,
     use_legacy_sql=False
 )
 
-#twitter_spark_etl = dagrun_operator.TriggerDagRunOperator(
+# twitter_spark_etl = dagrun_operator.TriggerDagRunOperator(
 #    task_id="execute_other_dag",
 #    trigger_dag_id="twitter_spark_etl",  # Ensure this equals the dag_id of the DAG to trigger
 #    conf={"job_date": "{{ ds }}"}, # No need for this parameter, please check execution_date
 #    execution_date="{{ ds }}",
 #    dag=dag,
-#)
+# )
 
-delete_sl_partition >> from_raw_to_sl
+bigquery_schema_update >> delete_sl_partition >> from_raw_to_sl
